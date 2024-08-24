2807e7603de2bc8d45f96cf6da09a80821ea38d3
DARK-WAY <o_kv@mail.ru>
2024-08-24 13:20:15 +0300

    Домашняя работа к Уроку 30

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -4,28 +4,23 @@ import ru.otus.java.basic.http.server.processors.AnotherHelloWorldRequestProcess
 import ru.otus.java.basic.http.server.processors.HelloWorldRequestProcessor;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
     private int port;
-    private Dispatcher dispatcher;
 
     public HttpServer(int port) {
         this.port = port;
-        this.dispatcher = new Dispatcher();
     }
-
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.printInfo(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ClientHandler(this, socket);
             }
         } catch (IOException e) {
             e.printStackTrace();
